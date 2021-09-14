95837013cd2665f562c3569848c74e0db09e2bac
pb2323 <pbajaj0023@gmail.com>
2021-09-14 23:12:05 +0530

    cleanup

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://mini-instagram-india.herokuapp.com";
+    : "https://slot-check-dashboard.herokuapp.com";
 
 module.exports = baseUrl;
